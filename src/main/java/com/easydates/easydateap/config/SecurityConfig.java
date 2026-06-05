54da94e449d434e810d1dbbb81b8698dca3a82f7
JuanitoEquisde <juanpazpastor@gmail.com>
2026-06-04 23:11:00 -0500

    cambios exitosos  26/05/26 21:16

--- src/main/java/com/easydates/easydateap/config/SecurityConfig.java
+++ src/main/java/com/easydates/easydateap/config/SecurityConfig.java
@@ -10,7 +10,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-//hol
+//ho
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
