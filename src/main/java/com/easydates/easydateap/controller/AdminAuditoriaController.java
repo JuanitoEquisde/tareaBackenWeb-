fd0f148b5cde684f70c2d072fd9f956482dd8e5a
JuanitoEquisde <juanpazpastor@gmail.com>
2026-06-04 23:03:47 -0500

    cambios exitosos  26/05/26 21:16

--- src/main/java/com/easydates/easydateap/controller/AdminAuditoriaController.java
+++ src/main/java/com/easydates/easydateap/controller/AdminAuditoriaController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequestMapping("/admin/auditoria")
 public class AdminAuditoriaController {
-
     @Autowired
     private IHistorialCambiosService historialService;
 
