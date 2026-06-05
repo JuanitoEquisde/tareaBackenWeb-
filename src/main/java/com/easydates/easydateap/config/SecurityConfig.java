86b5298cba36e2cf64e6d300809e10115c3859a8
JuanitoEquisde <juanpazpastor@gmail.com>
2026-06-04 23:00:11 -0500

    cambios exitosos  26/05/26 21:16

--- src/main/java/com/easydates/easydateap/config/SecurityConfig.java
+++ src/main/java/com/easydates/easydateap/config/SecurityConfig.java
@@ -15,7 +15,6 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
